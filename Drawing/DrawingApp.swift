fa348cefc97f9b2726d07f6b50e6de97697c7b5c
Ryan <ryan.minoo@wongfong.com>
2023-03-20 14:58:56 +0800

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Ryan on 20/3/23.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
