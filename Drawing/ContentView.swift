63aef8a02c6bbbb8657224114ad756f1f254e68d
Ryan <ryan.minoo@wongfong.com>
2023-03-20 15:20:48 +0800

    Shape testing

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -7,15 +7,33 @@
 
 import SwiftUI
 
+struct Arc : InsettableShape {
+    let startAngle : Angle
+    let endAngle : Angle
+    let clockWise : Bool
+    var insetAmount = 0.0
+    
+    func path(in rect: CGRect) -> Path {
+        
+        let angleRotation = Angle.degrees(90)
+        let modifiedStart = startAngle - angleRotation
+        let modifiedEnd = endAngle - angleRotation
+        var path = Path()
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 , startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockWise)
+        return path
+    }
+    
+    func inset(by amount: CGFloat) -> some InsettableShape {
+        var arc = self
+        arc.insetAmount += amount
+        return arc
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockWise: false)
+            .frame(width:300,height: 300)
     }
 }
 
