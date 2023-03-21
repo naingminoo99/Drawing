3481ad8df8617a2798394e7f29e93ed49d2b770b
Ryan <ryan.minoo@wongfong.com>
2023-03-21 09:19:58 +0800

    Flower Blossoming

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -30,17 +30,49 @@ struct Arc : InsettableShape {
     }
 }
 
-struct ContentView: View {
-    var body: some View {
-        Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockWise: false)
-            .stroke(.blue , lineWidth: 20)
-            .frame(width:300,height: 300)
+struct Flower : Shape {
+    var patelOffset = -20.0
+    var patelWidth = 100.0
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
         
+        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 8) {
+            let rotation = CGAffineTransform(rotationAngle: number)
+            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width/2, y: rect.height/2))
+            
+            let originalPatel = Path(ellipseIn: CGRect(x: patelOffset, y: 0, width: patelWidth, height: rect.width/2))
+            let rotatedPatel = originalPatel.applying(position)
+            
+            path.addPath(rotatedPatel)
+        }
+        return path
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct ContentView: View {
+    
+    @State private var patelOffset = -20.0
+    @State private var patelWidth = 100.0
+    
+    var body: some View {
+        VStack {
+            Flower(patelOffset: patelOffset, patelWidth: patelWidth)
+                .stroke(.red ,lineWidth: 1)
+            
+            Text("Offset")
+            Slider(value: $patelOffset , in: -40...40)
+                .padding([.horizontal,.vertical])
+            
+            Text("Width")
+            Slider(value: $patelWidth , in: 0...200)
+                .padding([.horizontal,.vertical])
+        }
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
