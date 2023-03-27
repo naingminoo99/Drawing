f55d2f2c2d910b2e6e15af536fd4487d7da50950
Ryan <ryan.minoo@wongfong.com>
2023-03-27 08:40:50 +0800

    Checkerboard with Animatable Data Pair

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -113,6 +113,40 @@ struct screenRenderCircle : View{
     }
 }
 
+struct CheckerBoard : Shape {
+    var rows : Int
+    var columns : Int
+    
+    var animatableData : AnimatablePair<Double,Double> {
+        get {
+            AnimatablePair(Double(rows), Double(columns))
+        }
+        set {
+            rows = Int(newValue.first)
+            columns = Int(newValue.second)
+        }
+    }
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        let rowSize = rect.height / Double(rows)
+        let columnSize = rect.width / Double(columns)
+                
+        for row in 0..<rows {
+            for column in 0..<columns {
+                if((row + column).isMultiple(of: 2)){
+                    let startX = columnSize * Double(column)
+                    let startY = rowSize * Double(row)
+                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
+                    path.addRect(rect)
+                }
+            }
+        }
+        return path
+    }
+}
+
 struct ContentView: View {
     
     @State private var patelOffset = -20.0
@@ -121,6 +155,9 @@ struct ContentView: View {
     @State private var colorCycle = 0.0
     
     @State private var amount = 0.0
+    
+    @State private var rows = 4
+    @State private var column = 4
     
     var body: some View {
         //        VStack {
@@ -161,7 +198,13 @@ struct ContentView: View {
 //                .padding([.horizontal,.vertical])
 //
 //        }
-        
+        CheckerBoard(rows: rows, columns: column)
+            .onTapGesture {
+                withAnimation(.linear(duration: 3 )) {
+                    rows = 8
+                    column = 8
+                }
+            }
     }
     
     struct ContentView_Previews: PreviewProvider {
